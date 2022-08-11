3ff9af4fa840e86f36cacbb6efe35a3ceb14f7f4
Matthew Oluwabusayo Opoola <initoluwa@gmail.com>
2022-08-11 11:20:58 +0100

    First commit

---
+++ 2-global.js
@@ -0,0 +1,13 @@
+//GLOBALS - NO WINDOW !!!!
+
+// __dirname - path to current directory
+// __filename - file name
+// require - function to use modules (CommonJs)
+// module - info about current module (file)
+// process - info about where the program is being executed
+
+console.log(process)
+
+setInterval(() =>{
+    console.log('hello world')
+}, 1000)
---
+++ 3-module.js
@@ -0,0 +1,10 @@
+// Modules
+const names = require('./4-name');
+const sayHi = require('./5-utils');
+const data = require('./6-alternative');
+require('./7-mind')
+console.log(data);
+
+sayHi('Matthew')
+sayHi(names.john)
+sayHi(names.peter)
