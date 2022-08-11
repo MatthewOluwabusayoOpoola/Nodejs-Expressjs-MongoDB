e48ea47598e5ca0e6e1cfbf4fd69e3a41b9bb539
Matthew Oluwabusayo Opoola <initoluwa@gmail.com>
2022-08-11 20:55:45 +0100

    new commit

--- 11-fs-async.js
+++ 11-fs-async.js
@@ -24,3 +24,38 @@ writeFile(
 })
 })
 console.log('starting new task');
+
+
+// const { readFile, writeFile } = require('fs').promises
+// const util = require('util')
+// const readFilePromise = util.promisify(readFile)
+// const writeFilePromise = util.promisify(writeFile)
+
+// const start = async() =>{
+//     try {
+//         const first = await readFile('./content/first.txt','utf8');
+//         const second = await readFile('./content/second.txt','utf8');
+//         await writeFile('./content/result-mind-grenade', 
+//         `THIS IS SO CRAZY: ${first} ${second} `,{flag: 'a'})
+//         console.log(first,second);
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+
+// start()
+
+// const getText = (path) => {
+//     return new Promise((resolve, reject) => {
+//         readFile(path, 'utf8', (err, data) => {
+//             if (err) {
+//                 reject(err)
+//             } else {
+//                 resolve(data)
+//             }
+//         })
+//     })
+// }
+// getText('./content/first.txt')
+// .then((result) => console.log(result))
+// .catch((err) => console.log(err))
