b968b0d5617ddb817593a12e1155931290ca635b
Matthew Oluwabusayo Opoola <initoluwa@gmail.com>
2022-08-14 18:46:05 +0100

    fourth commit

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require('express')
 const app = express()
-let { people } = require('./data')
 
+const people = require('./route/people')
+const auth = require('./route/auth')
 
+// static assets
+app.use(express.static('./methods-public'))
+//  parse data form
+app.use(express.urlencoded({extended: false}))
+// parse.json
+app.use(express.json())
 
-app.get('/api/people', (req,res) => {
-    res.status(200).json({ success: true, data: people})
-})
+app.use('/api/people', people )
+
+app.use('/login', auth)
+
 app.listen(5000, () => {
     console.log("Server is listening at port 5000.....");
 })
